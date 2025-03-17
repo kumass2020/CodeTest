6cef4b90214af8a8c88f9929268869d327ec1843
hoho <kumass2021@gmail.com>
2025-03-17 20:44:52 +0900

    [level 3] Title: 양과 늑대, Time: 9.03 ms, Memory: 9.09 MB -BaekjoonHub

---
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
@@ -0,0 +1,117 @@
+# from collections import deque
+
+# def solution(info, edges):
+#     answer = 0
+    
+#     child = [[] for i in range(17)]
+#     parent = [0 for i in range(17)]
+    
+    
+#     for i, edge in enumerate(edges):
+#         idx = edge[0]
+#         child_num = edge[1]
+        
+#         child[idx].append(child_num)
+#         parent[child_num] = idx
+        
+#     # (idx, cost)
+#     q = deque()
+#     q.append((0, 0))
+#     res_sheep = 0
+#     sheep = 0
+    
+#     cost = [0 for i in range(len(info))]
+#     visited = [0 for i in range(len(info))]
+    
+#     while q:
+#         c_idx, res_sheep = q.popleft()
+#         # c_idx = c_info
+#         visited[c_idx] = 1
+#         cost[c_idx] = 99
+
+#         if info[c_idx] == 0:
+#             res_sheep += 1
+#             sheep += 1
+#         else:
+#             res_sheep -= 1
+
+#         # 모든 노드를 돌면서 양까지 닿는 cost를 업데이트
+#         def update_subtree(c_idx):
+#             for idx in child[c_idx]:
+#                 if info[c_idx] == 0:
+#                     cost[idx] -= 1
+#                 else:
+#                     cost[idx] += 1
+#                 update_subtree(idx)
+                
+#         if len(q) == 0:
+#             min_cost = min(cost)
+#             min_cost_idx = cost.index(min_cost)
+
+#             idx = min_cost_idx
+#             while idx != c_idx:
+#                 if not visited[idx]:
+#                     q.appendleft((idx, res_sheep))
+#                     idx = parent[idx]
+                
+# #     for i in range(0, len(info)):
+# #         parent_idx = parent[i]
+# #         j = i
+# #         res_sheep = 0
+        
+# #         while True:
+# #             if j == 0:
+# #                 cost[i] = res_sheep
+# #                 break
+                
+# #             if info[j] == 0:
+# #                 res_sheep += 1
+# #             else:
+# #                 res_sheep -= 1
+# #             j = parent[j]
+        
+# #         if sheep <= wolf:
+# #             break
+        
+# #         for idx in child[i]:
+# #             if child[idx] == 0:
+# #                 q.appendleft((idx, info[idx]))
+# #                 continue
+# #             elif child[idx] == 1:
+# #                 q.append((idx, info[idx]))
+# #                 continue
+        
+#     answer = sheep
+#     return answer
+
+def solution(info, edges):
+    n = len(info)
+    graph = [[] for _ in range(n)]
+    
+    for parent, child in edges:
+        graph[parent].append(child)
+    
+    answer = 0 
+    
+    def dfs(sheep, wolf, candidates):
+        nonlocal answer
+        
+        answer = max(answer, sheep)
+        
+        for i, node in enumerate(candidates):
+            ns, nw = sheep, wolf
+            if info[node] == 0:
+                ns += 1
+            else:
+                nw += 1
+            if nw >= ns:
+                continue
+                
+            new_candidates = candidates[:]
+            new_candidates.remove(node)
+            new_candidates.extend(graph[node])
+            
+            dfs(ns, nw, new_candidates)
+    
+    dfs(1, 0, graph[0])
+    return answer
