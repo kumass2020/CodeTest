3f50aa518008ac84f38ed6c6e24f0d56ef8b2ac5
hoho <kumass2021@gmail.com>
2025-04-04 12:51:43 +0900

    Time: 1 ms (61.07%), Space: 19.1 MB (9.08%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,14 +1,44 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        memory = 101
-        k = len(nums)
-        for i in range(len(nums)):
-            num = nums[i]
-            if memory == num:
-                nums[i] = 101
-                k -= 1
-            memory = num
+        # memory = 101
+        # k = len(nums)
+        # for i in range(len(nums)):
+        #     num = nums[i]
+        #     if memory == num:
+        #         nums[i] = 101
+        #         k -= 1
+        #     memory = num
 
-        nums.sort()
+        # nums.sort()
 
-        return k
+        # return k
+        
+        # L = len(nums)
+
+        # # i = 0
+        # j = 0
+        # memory = 101
+        # for i in range(len(nums)):
+        #     if nums[j] == memory:
+        #         nums[j] = '_'
+
+        #     while j < len(nums)-1 and nums[j] != '_':
+        #         j += 1
+
+        #     if i+1 < len(nums):
+        #         nums[i+1] = nums[j]
+        #         nums[j] = '_'
+
+        #     if nums[i] == '_':
+        #         return i
+
+        #     memory = nums[j]
+
+        j = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i-1]:
+                nums[j] = nums[i]
+                j += 1
+        
+        return j
+            
