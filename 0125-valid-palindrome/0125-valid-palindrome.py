06e612bd1922e6ff9cb40df3e9e0985ada495e9b
hoho <kumass2021@gmail.com>
2025-04-05 12:49:40 +0900

    Time: 27 ms (5.18%), Space: 18.3 MB (43.25%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,24 +1,27 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        # regex approach
+        # s = re.sub('[^a-zA-Z0-9]', '', s).lower()     # -> amanaplanacanalpanama
+
         # s = s.replace(' ', '')
         i = 0
         j = len(s)-1
 
         while i <= j:
-            while i <= j and not s[i].isalnum():
+            while i < j and not s[i].isalnum():
                 i += 1
-            while i <= j and not s[j].isalnum():
+            while i < j and not s[j].isalnum():
                 j -= 1
             
             print(i, j)
 
-            if i > j:
-                return True
+            # if i > j:
+            #     return True
 
             if s[i].upper() != s[j].upper():
                 return False
-            else:
-                i += 1
-                j -= 1
+            
+            i += 1
+            j -= 1
 
         return True
