8a0ac40b1a751f305ede08d404b7a9b9faf5fb68
hoho <kumass2021@gmail.com>
2025-04-05 19:53:40 +0900

    Time: 288 ms (7.92%), Space: 59.9 MB (16.24%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,9 +1,30 @@
 from itertools import combinations
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        if n == 1:
-            return [[n]]
-        lst = [i for i in range(1, n+1)]
-        combs = list(combinations(lst, k))
-        return combs
+        ## using itertools.combniations
+        # if n == 1:
+        #     return [[n]]
+        # lst = [i for i in range(1, n+1)]
+        # combs = list(combinations(lst, k))
+        # return combs
+
+
+        # using DFS
+        answer = []
+        def dfs(idx, comb):
+            # print(comb)
+            nonlocal answer
+            
+            if len(comb) == k:
+                print(comb)
+                answer.append(comb[:])
+                return
+
+            for i in range(idx, n+1):
+                comb.append(i)
+                dfs(i+1, comb)
+                comb.pop()
+
+        dfs(1, [])
+        return answer
         
