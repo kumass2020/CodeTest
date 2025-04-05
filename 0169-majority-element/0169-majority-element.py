b66c4ce98c1e3809ca199ac0e6be30f73752d07f
hoho <kumass2021@gmail.com>
2025-04-05 14:54:13 +0900

    Time: 2 ms (84.35%), Space: 19.3 MB (87.4%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,11 +1,15 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums2 = []
-        for i in range(len(nums)):
-            if nums2 and nums2[-1] != nums[i]:
-                nums2.pop()
-            else:
-                nums2.append(nums[i])
+        # nums2 = []
+        # for i in range(len(nums)):
+        #     if nums2 and nums2[-1] != nums[i]:
+        #         nums2.pop()
+        #     else:
+        #         nums2.append(nums[i])
         
-        return nums2[-1]
+        # return nums2[-1]
+
+        # Sorting
+        nums.sort()
+        return nums[len(nums)//2]
             
