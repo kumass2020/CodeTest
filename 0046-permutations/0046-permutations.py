4c8277a4ea2b3ac7892cbba7e1bae6406c48fc69
hoho <kumass2021@gmail.com>
2025-04-05 20:14:32 +0900

    Time: 4 ms (12.36%), Space: 18 MB (32.49%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,5 +1,22 @@
 from itertools import permutations
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        perms = permutations(nums)
-        return list(perms)
+        ## Using itertools.permutations
+        # perms = permutations(nums)
+        # return list(perms)
+
+        # DFS
+        answer = []
+
+        def dfs(idx, perm):
+            if len(perm) == len(nums):
+                answer.append(perm[:])
+
+            for i in range(len(nums)):
+                if nums[i] not in perm:
+                    perm.append(nums[i])
+                    dfs(i+1, perm)
+                    perm.pop()
+
+        dfs(0, [])
+        return answer
