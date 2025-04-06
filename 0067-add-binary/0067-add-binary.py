23fe4caeca899828e3f705378f41dd713249fb77
hoho <kumass2021@gmail.com>
2025-04-06 21:18:42 +0900

    Time: 3 ms (44.36%), Space: 17.9 MB (51.82%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -6,22 +6,46 @@ class Solution:
         #     if c[i] ==
         #     answer = c[i] + answer 
 
-        def get_10(binary):
-            j = 0
-            num = 0
-            for i in range(len(binary)-1, -1, -1):
-                num += int(binary[i]) * (2**(j))
-                j += 1
-            return num
+        # def get_10(binary):
+        #     j = 0
+        #     num = 0
+        #     for i in range(len(binary)-1, -1, -1):
+        #         num += int(binary[i]) * (2**(j))
+        #         j += 1
+        #     return num
         
-        def get_2(num):
-            answer = ''
-            while num > 0:
-                answer = str(num % 2) + answer
-                num = num // 2
-            return answer if answer else '0'
+        # def get_2(num):
+        #     answer = ''
+        #     while num > 0:
+        #         answer = str(num % 2) + answer
+        #         num = num // 2
+        #     return answer if answer else '0'
 
-        a = get_10(a)
-        b = get_10(b)
+        # a = get_10(a)
+        # b = get_10(b)
 
-        return get_2(a + b)
+        # return get_2(a + b)
+
+        carry = 0
+        i = len(a) - 1
+        j = len(b) - 1
+        answer = ''
+        while i >= 0 or j >= 0:
+            summ = 0
+            summ += int(a[i]) if i >= 0 else 0
+            summ += int(b[j]) if j >= 0 else 0
+            summ += carry
+            
+            if summ >= 2:
+                carry = 1
+                summ -= 2
+            else:
+                carry = 0
+            answer = str(summ) + answer
+            i -= 1
+            j -= 1
+        if carry:
+            answer = '1' + answer 
+        
+        return answer
+                
