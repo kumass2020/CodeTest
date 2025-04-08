f6bd6a83f5d49cad8b8d90233f8e1ee9c8942c3e
hoho <kumass2021@gmail.com>
2025-04-08 12:47:38 +0900

    Time: 4 ms (57.68%), Space: 25.5 MB (74.28%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -4,10 +4,21 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        # k = k % len(nums)
+        # q = deque(nums)
+        # for i in range(k):
+        #     q.appendleft(q[-1])
+        #     q.pop()
+        #     # print(q)
+        # nums[:] = list(q)
+
         k = k % len(nums)
-        q = deque(nums)
-        for i in range(k):
-            q.appendleft(q[-1])
-            q.pop()
-            # print(q)
-        nums[:] = list(q)
+        # temp = [0 for _ in range(k)]
+        
+        temp = nums[-k:]
+        for i in range(len(nums)-1, k-1, -1):
+            nums[i] = nums[i-k]
+        for j in range(k):
+            nums[j] = temp[j]
+
+        
