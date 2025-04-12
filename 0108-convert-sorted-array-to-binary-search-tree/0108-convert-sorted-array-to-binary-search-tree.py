5e5eabd63f2063d7525899cfc672fb57948cda12
hoho <kumass2021@gmail.com>
2025-04-12 18:31:10 +0900

    Time: 6 ms (5.83%), Space: 18.8 MB (98.82%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.py
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.py
@@ -6,30 +6,44 @@
 #         self.right = right
 class Solution:
     def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
-        
-        N = len(nums)
-        mid = N // 2
-        def traverse(parent, lst):
-            idx = (len(lst) - 1) // 2
-            # print(lst, idx)
-            if not lst:
-                return
-            new = TreeNode(lst[idx])
-            if lst[idx] < parent.val:
-                parent.left = new
-            else:
-                parent.right = new
-            left_lst = lst[:idx]
-            right_lst = lst[idx+1:]
-            parent = new
-            if left_lst:
-                traverse(parent, left_lst)
-            if right_lst:
-                traverse(parent, right_lst)
+        if not nums:
+            return
 
+        mid = (len(nums) - 1) // 2
         root = TreeNode(nums[mid])
-        traverse(root, nums[:mid])
-        traverse(root, nums[mid+1:])
+
+        root.left = self.sortedArrayToBST(nums[:mid])
+        root.right = self.sortedArrayToBST(nums[mid+1:])
+
         return root
+
+
+        
+        # N = len(nums)
+        # mid = N // 2
+        # def traverse(parent, lst):
+        #     idx = (len(lst) - 1) // 2
+        #     # print(lst, idx)
+        #     if not lst:
+        #         return
+        #     new = TreeNode(lst[idx])
+        #     if lst[idx] < parent.val:
+        #         parent.left = new
+        #     else:
+        #         parent.right = new
+        #     left_lst = lst[:idx]
+        #     right_lst = lst[idx+1:]
+        #     parent = new
+        #     if left_lst:
+        #         traverse(parent, left_lst)
+        #     if right_lst:
+        #         traverse(parent, right_lst)
+
+        # root = TreeNode(nums[mid])
+        # traverse(root, nums[:mid])
+        # traverse(root, nums[mid+1:])
+        # return root
+
+
 
             
