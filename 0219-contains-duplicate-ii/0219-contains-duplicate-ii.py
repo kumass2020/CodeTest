9d616670152d1201418d35ca20dfe98c5ed56c30
hoho <kumass2021@gmail.com>
2025-04-14 19:28:59 +0900

    Time: 103 ms (5.01%), Space: 47.1 MB (5.61%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -3,13 +3,21 @@ class Solution:
         dt = defaultdict(list)
 
         for i in range(len(nums)):
-            if dt.get(nums[i]):
-                nums2 = dt.get(nums[i])
-                for num2 in nums2:
-                    if abs(i-num2) <= k:
-                        return True
+            # if dt.get(nums[i]):
+            #     nums2 = dt.get(nums[i])
+            #     for num2 in nums2:
+            #         if abs(i-num2) <= k:
+            #             return True
 
             dt[nums[i]].append(i)
+
+        for key, value in dt.items():
+            N = len(value)
+            if N >= 2:
+                for i in range(N):
+                    for j in range(i+1, N):
+                        if abs(value[i] - value[j]) <= k:
+                            return True
 
         return False
 
