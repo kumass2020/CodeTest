55b84df8d78a376a48b3e0043e6e144cf3221bad
hoho <kumass2021@gmail.com>
2025-04-15 21:08:22 +0900

    Time: 125 ms (66.25%), Space: 58 MB (38.8%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -2,27 +2,41 @@ import random
 class RandomizedSet:
 
     def __init__(self):
-        self.set = set()
+        # self.set = set()
+        self.dt = defaultdict(int)
         self.lst = []
+        # self.count = 0
 
     def insert(self, val: int) -> bool:
-        if val not in self.set:
-            self.set.add(val)
+        if val not in self.dt:
+            # self.set.add(val)
+            # self.count += 1
+            self.dt[val] = len(self.lst)
             self.lst.append(val)
             return True
         else:
             return False        
 
     def remove(self, val: int) -> bool:
-        if val in self.set:
-            self.set.remove(val)
-            self.lst.remove(val)
+        # print(val, self.dt)
+        if val in self.dt:
+            # self.set.remove(val)
+            last_elem = self.lst[-1]
+            idx = self.dt[val]
+
+            # elem_to_rm = self.lst[idx]
+            self.lst[idx] = last_elem
+            self.dt[last_elem] = idx
+            
+            self.lst.pop()
+            self.dt.pop(val)
             return True
         else:
             return False
 
     def getRandom(self) -> int:
-        return self.lst[random.randint(0, len(self.set)-1)]
+        # return self.lst[random.randint(0, len(self.set)-1)]
+        return random.choice(self.lst)
 
 
 # Your RandomizedSet object will be instantiated and called as such:
