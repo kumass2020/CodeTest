e1729d9e7ff37f45904195bf39783d996ac6bde0
hoho <kumass2021@gmail.com>
2025-04-18 18:22:17 +0900

    Time: 14 ms (20.57%), Space: 18 MB (19.69%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,19 +1,31 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if 0 <= x <= 9:
-            return True
-        elif x < 0:
+        # if 0 <= x <= 9:
+        #     return True
+        # elif x < 0:
+        #     return False
+        
+        # orig_x = x
+        # num = 0
+        # i = 0
+        # # while x != 0:
+        # #     # print((10**i) * (x % 10))
+        # #     # num += (10**i) * (x % 10)
+        # #     num.append(str(x % 10))
+        # #     x //= 10
+        # #     i += 1
+        
+        # return orig_x == int(''.join(num))
+
+        if x < 0:
             return False
         
-        orig_x = x
-        num = []
-        i = 0
-        while x != 0:
-            # print((10**i) * (x % 10))
-            # num += (10**i) * (x % 10)
-            num.append(str(x % 10))
-            x //= 10
-            i += 1
+        x_cp = x
+        num = 0
+        # i = 0
+        while x_cp != 0:
+            # print(10 * num, x_cp % 10)
+            num = 10 * num + x_cp % 10
+            x_cp //= 10
         
-        return orig_x == int(''.join(num))
-
+        return x == num
