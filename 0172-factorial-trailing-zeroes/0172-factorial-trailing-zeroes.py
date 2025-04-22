f15ccf1461ec9d6f1308edfe0798ee84a16772e5
hoho <kumass2021@gmail.com>
2025-04-22 22:25:08 +0900

    Time: 3 ms (35.64%), Space: 17.9 MB (21.09%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
@@ -2,8 +2,8 @@ class Solution:
     def trailingZeroes(self, n: int) -> int:
         # if n == 0:
         #     return 0
-        if n <= 4:
-            return 0
+        # if n <= 4:
+        #     return 0
         
         # num = 1
         # count = 0
@@ -16,13 +16,35 @@ class Solution:
         
         # return count
 
-        count = 0
-        for i in range(5, n+1, 5):
-            count += 1
+        # count = 0
+        # for i in range(5, n+1, 5):
+        #     count += 1
 
-            for j in [5, 4, 3, 2]:
-                if i % 5**j == 0:
-                    count += j-1
-                    break
+        #     for j in [5, 4, 3, 2]:
+        #         if i % 5**j == 0:
+        #             count += j-1
+        #             break
 
-        return count
+        # return count
+
+        def get_zero(n):
+            if n <= 4:
+                return 0
+            exp = 0
+            coeff = 0
+            for i in range(6):
+                if 5**i <= n < 5**(i+1):
+                    exp = i
+                    coeff = n // (5**i)
+
+            # print(n, exp, coeff)
+            count = 0
+            for i in range(exp):
+                count += coeff * (5**i)
+
+                if i == exp-1:
+                    # count += (n - coeff*(5**i)) // 5
+                    count += get_zero(n - coeff*(5**(i+1)))
+        
+            return count
+        return get_zero(n)
