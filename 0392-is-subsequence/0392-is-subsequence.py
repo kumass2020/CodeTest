11e6f99b7138f93e4e88bdae9a3012ffebf31571
hoho <kumass2021@gmail.com>
2025-06-13 11:59:56 +0900

    Time: 0 ms (100%), Space: 17.8 MB (67.08%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,27 +1,48 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        idx = -1
-        # for c in s:
-        #     print(idx, t)
-        #     c_idx = t.index(c)
-        #     if idx >= c_idx:
-        #         return False
-        #     idx = c_idx
-        # return True
-
-        if len(s) == 0:
+        if not s:
             return True
-        if len(t) == 0 and len(s) > 0:
+        elif not s or not t:
             return False
 
-        i = 0
-        for c in t:
-            # print(c)
-            if c == s[i]:
-                # print(s[i], i)
+        i = j = 0
+
+        while i <= len(s)-1 and j <= len(t)-1:
+            if s[i] == t[j]:
                 i += 1
-                if i == len(s):
-                    return True
-        return False
+                j += 1
+            else:
+                j += 1
+
+        # print(i, j)
+
+        if i != len(s):
+            return False
+        elif i >= len(s):
+            return True
+
+        # idx = -1
+        # # for c in s:
+        # #     print(idx, t)
+        # #     c_idx = t.index(c)
+        # #     if idx >= c_idx:
+        # #         return False
+        # #     idx = c_idx
+        # # return True
+
+        # if len(s) == 0:
+        #     return True
+        # if len(t) == 0 and len(s) > 0:
+        #     return False
+
+        # i = 0
+        # for c in t:
+        #     # print(c)
+        #     if c == s[i]:
+        #         # print(s[i], i)
+        #         i += 1
+        #         if i == len(s):
+        #             return True
+        # return False
             
         
