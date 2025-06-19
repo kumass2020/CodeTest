4e84baf011814b5644ee63a0fa7168608f4efc4c
hoho <kumass2021@gmail.com>
2025-06-19 23:54:34 +0900

    Time: 2 ms (75.37%), Space: 18.6 MB (16.57%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -6,18 +6,31 @@ class Solution:
         M = len(matrix)
         N = len(matrix[0])
 
-        visited = [[False] * N for _ in range(M)]
-
+        zeros = []
         for i in range(M):
             for j in range(N):
-                if matrix[i][j] == 0 and not visited[i][j]:
-                    for m in range(M):
-                        if matrix[m][j] != 0:
-                            visited[m][j] = True
-                        matrix[m][j] = 0
+                if matrix[i][j] == 0:
+                    zeros.append((i, j))
+
+        for i, j in zeros:
+            for m in range(M):
+                matrix[m][j] = 0
+
+            for n in range(N):
+                matrix[i][n] = 0
+                
+        # visited = [[False] * N for _ in range(M)]
+
+        # for i in range(M):
+        #     for j in range(N):
+        #         if matrix[i][j] == 0 and not visited[i][j]:
+        #             for m in range(M):
+        #                 if matrix[m][j] != 0:
+        #                     visited[m][j] = True
+        #                 matrix[m][j] = 0
 
 
-                    for n in range(N):
-                        if matrix[i][n] != 0:
-                            visited[i][n] = True
-                        matrix[i][n] = 0
+        #             for n in range(N):
+        #                 if matrix[i][n] != 0:
+        #                     visited[i][n] = True
+        #                 matrix[i][n] = 0
