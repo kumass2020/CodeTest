30f32c5e185bdc01d8e057ef01e3717c80128868
hoho <kumass2021@gmail.com>
2025-06-19 23:41:35 +0900

    Time: 6 ms (21.94%), Space: 18.7 MB (9.7%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -0,0 +1,23 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        M = len(matrix)
+        N = len(matrix[0])
+
+        visited = [[False] * N for _ in range(M)]
+
+        for i in range(M):
+            for j in range(N):
+                if matrix[i][j] == 0 and not visited[i][j]:
+                    for m in range(M):
+                        if matrix[m][j] != 0:
+                            visited[m][j] = True
+                        matrix[m][j] = 0
+
+
+                    for n in range(N):
+                        if matrix[i][n] != 0:
+                            visited[i][n] = True
+                        matrix[i][n] = 0
