db35192d7a82074e9559838128929d23396a7acf
hoho <kumass2021@gmail.com>
2025-07-01 15:26:17 +0900

    Time: 1 ms (9.25%), Space: 17.9 MB (34.71%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -8,28 +8,58 @@ class Solution:
         if not head.next:
             return None
         
-        lst = []
+        counter = 0
+        def traverse(left, right):
+            nonlocal head, counter
 
-        def traverse(node):
-            nonlocal lst
+            if counter == n:
+                left = head
 
-            if not node:
+            if not right.next:
+                if n == 1:
+                    left.next = None
+                elif not left:
+                    head = head.next
+                else:
+                    left.next = left.next.next
+
                 return
 
-            lst.append(node)
+            counter += 1
 
-            traverse(node.next)
-        
-        traverse(head)
-        
-        N = len(lst)
-        idx = N-n
-        if n == 1:
-            lst[idx-1].next = None
-        elif n == len(lst):
-            head = lst[1]
-        else:
-            # print(idx, lst)
-            lst[idx-1].next = lst[idx+1]
-        
+            if left:
+                traverse(left.next, right.next)
+            else:
+                traverse(None, right.next)
+
+        traverse(None, head)
+
         return head
+            
+
+            
+        # lst = []
+
+        # def traverse(node):
+        #     nonlocal lst
+
+        #     if not node:
+        #         return
+
+        #     lst.append(node)
+
+        #     traverse(node.next)
+        
+        # traverse(head)
+        
+        # N = len(lst)
+        # idx = N-n
+        # if n == 1:
+        #     lst[idx-1].next = None
+        # elif n == len(lst):
+        #     head = lst[1]
+        # else:
+        #     # print(idx, lst)
+        #     lst[idx-1].next = lst[idx+1]
+        
+        # return head
